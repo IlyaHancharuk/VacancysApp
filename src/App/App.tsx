824183e3f5bbfc9f50d169f32c3bfc6713e36d08
Ilya Hancharuk <innot707@gmail.com>
2023-05-16 10:36:06 +0300

    feat: add styles to header

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { MainPAge } from '../pages/MainPage';
-import Header from '../components/Header';
+import Header from '../components/Header/Header';
 import { Routes, Route } from "react-router-dom";
 import { FavoritesPAge } from '../pages/FavoritesPage';
 import { VacancyPAge } from '../pages/Vacancy';
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,46 @@
+import React, { FC } from "react";
+import { NavLink } from "react-router-dom";
+import './header.css'
+
+type HeaderPropsType = {
+    
+}
+
+const Header: FC<HeaderPropsType> = (props) => {
+
+    const navLinks = [
+        ['vacancies', 'Поиск Вакансий'],
+        ['favorites', 'Избранное']
+    ]
+
+    const linksList = navLinks.map((link) => (
+        <li className="menu__item">
+            <NavLink to={link[0]}
+                     className={({ isActive }) => isActive ? "active" : ""}
+            >
+                {link[1]}
+            </NavLink>
+        </li>
+    ))
+    
+    return (
+        <div className="header-container">
+            <header className="header">
+                <div className="logo">
+                    <div className="logo__icon">
+                        <div className="ellipse ellipse-1"></div>
+                        <div className="ellipse ellipse-2"></div>
+                    </div>
+                    <div className="logo__title">Jobored</div>
+                </div>
+                <nav className="menu">
+                    <ul className="menu__list">
+                        {linksList}
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
